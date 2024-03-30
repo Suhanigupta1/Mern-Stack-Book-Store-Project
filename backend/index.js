b38147bd7001218eeb2ad296dc7c018dfde7b410
Suhani Gupta <114853473+Suhanigupta1@users.noreply.github.com>
2024-03-30 21:08:39 +0530

    Create index.js
    index.js file created

---
+++ backend/index.js
@@ -0,0 +1,37 @@
+import express, { response } from "express";
+import mongoose from "mongoose";
+import {Book} from './models/bookModel.js'
+import booksRoute from "../backend/routes/booksRoute.js"
+const app = express();
+import {PORT, mongoDBURL } from "./config.js";
+app.use(express.json());
+import cors from 'cors';
+//Middleware for handling CORS POLICY
+//Option 1 : Allow All origins with Default of cors;
+app.use(cors());
+// // Option 2: Allow Custom Origins
+// app.use(
+//   cors({
+//     origin:'http://localhost:3000' ,
+//     methods: ['GET','POST' ,'PUT', 'DELETE'],
+//     allowedHeaders: ['Content-Type'],  
+//   })
+// );
+app.get('/', (request, response)=>{
+  console.log(request);
+  return response.status(234).send('Welcome to mern Stack tutorial')
+})
+
+app.use('/books', booksRoute);
+
+
+
+mongoose.connect(mongoDBURL).then(()=>{
+    console.log('App connected to database');
+    app.listen(PORT, ()=>{
+        console.log(`App is listenting to port: ${PORT}`);
+    }); 
+})
+.catch((error)=>{
+    console.log(error);
+});
